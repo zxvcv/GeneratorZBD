92018673d85a286f0b272180ed72efba4755a857
Pawel Piskorz <ppiskorz0@gmail.com>
2020-11-22 22:42:11 +0100

    v2 generation works

--- libs/GeneratorCSV.py
+++ libs/GeneratorCSV.py
@@ -1,31 +1,50 @@
 import csv
+import sys
 import json
 import time
 import random
+import string
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from datetime import datetime
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 
 
 class GeneratorCSV:
     def __init__(self, settingsFile):
-        self.csvfile = open("data.csv", "w")
-        self.writer = csv.writer(self.csvfile, delimiter=';')
-        
         self.settings = []
         with open(settingsFile, "r") as f:
             self.settings = json.loads(f.read())
 
+        self.csvClientFile = open(self.settings["client_data_file"], "w", newline='')
+        self.clientWriter = csv.writer(self.csvClientFile, delimiter=';')
+
+        self.csvHireFile = open(self.settings["hire_data_file"], "w", newline='')
+        self.hireWriter = csv.writer(self.csvHireFile, delimiter=";")
+
+        self.carID = 0
+        self.clientID = 0
+
+        self.clientKeys = ["name","surname","birthDate","gender","maritialStatus",
+            "numberOfChildrens","priceFactor"]
+        self.clientWriter.writerow(self.clientKeys)
+
+        self.hireKeys = ["hireStartTime","hireEndTime","carId","clientId","clientSatisfaction",
+            "roadLength","repairsCost"]
+        self.hireWriter.writerow(self.hireKeys)
+
 
     def __del__(self):
-        self.csvfile.close()
+        self.csvClientFile.close()
+        self.csvHireFile.close()
 
 
-    def __str_time_prop(self, start, end, format, prop):
+    def __str_time_prop(self, start, end, prop):
         """
             Get a time at a proportion of a range of two formatted times.
 
@@ -34,12 +53,10 @@ class GeneratorCSV:
             prop specifies how a proportion of the interval to be taken after
             start.  The returned time will be in the specified format.
         """
-        stime = time.mktime(time.strptime(start, format))
-        etime = time.mktime(time.strptime(end, format))
+        delta = end - start
+        ptime = start + timedelta(seconds=prop * delta.total_seconds())
 
-        ptime = stime + prop * (etime - stime)
-
-        return time.strftime(format, time.localtime(ptime))
+        return ptime
 
 
     def __get_file_lines(self, file_name, lines_to_read):
@@ -47,81 +64,133 @@ class GeneratorCSV:
         with open(file_name, "r") as file:
             for position, line in enumerate(file):
                 if position in lines_to_read:
-                    found_lines.append(line)
+                    found_lines.append(line.replace("\"", "").rstrip("\n"))
         return found_lines
 
 
     def __hire_generate(self):
         # helpers
-        max_hires_for_client = self.settings["max_hires_for_client"]
-        max_hire_length_months = self.settings["max_hire_length_months"]
-        start_hire_minus_delta_days = self.settings["start_hire_minus_delta_days"]
-        date_format = self.settings["date_format"]
-        datetime_helper1 = datetime.strptime(client["birthDate"], date_format)
+        _max_hires_for_client = self.settings["max_hires_for_client"]
+        _max_hire_length_months = self.settings["max_hire_length_months"]
+        _start_hire_minus_delta_days = self.settings["start_hire_minus_delta_days"]
+        _min_hire_time_hours = self.settings["min_hire_time_hours"]
+        _date_format = self.settings["date_format"]
+
+        # other items generation
+        client = self.__client_generate()
+
+        #car = self.__car_get() #TODO: poyslec nad generacja auta tak zeby to mialo sens
 
         #generation
         client = self.__client_generate()
-        for i in range(random.randrange(1, max_hires_for_client + 1)):
-            hireStartTime = self.__str_time_prop(datetime_helper1 + timedelta(years=18)
+        for i in range(random.randrange(1, _max_hires_for_client + 1)):
+            hireStartTime = self.__str_time_prop(client["birthDate"] + relativedelta(years=18), 
+                datetime.now() - timedelta(days=_start_hire_minus_delta_days), random.random())
+            __helper = self.__str_time_prop(hireStartTime + relativedelta(hours=_min_hire_time_hours), 
+                hireStartTime + relativedelta(months=_max_hire_length_months), random.random())
+            hireEndTime = datetime.now() if __helper > datetime.now() else  __helper
+            carId = 0#car["id"]
+            clientId = client["id"]
+            clientSatisfaction = random.randrange(0, 11)
+            __helper3 = hireEndTime - hireStartTime
+            roadLength = __helper3.days * float(random.randrange(0, 1000)) / 10
+            __helper2 = 1 if random.randrange(0, 100) == 0 else 0
+            repairsCost = __helper2 * random.randrange(0, 200) * 100
+            
+            
 
+            # to collection
+            data = {
+                "hireStartTime": hireStartTime,
+                "hireEndTime": hireEndTime,
+                "carId": 0,#carID,
+                "clientId": clientId,
+                "clientSatisfaction": clientSatisfaction,
+                "roadLength": roadLength,
+                "repairsCost": repairsCost
+            }
 
+            # asserts
+            assert (data["hireStartTime"] >= datetime.strptime(self.settings["birth_first_date"], _date_format)),"Hire Start Time is too small"
+            assert (data["hireStartTime"] <= datetime.now() - relativedelta(days=_start_hire_minus_delta_days)), "Hire Start Time is too big"
+            assert (data["hireEndTime"] >= datetime.strptime(self.settings["birth_first_date"], _date_format)), "Hire End Time is too small"
+            assert (data["hireEndTime"] <= datetime.now()), "Hire End Time is too big"
+            assert (data["carId"] >= 0), "CarID cant be negative"
+            assert (data["clientId"] >= 0), "ClientID cant be negative"
+            assert (data["clientSatisfaction"] >= 0), "Clients' satisfaction cant be negative"
+            assert (data["clientSatisfaction"] <= 10), "Clients' satisfaction cant be bigger than 10"
+            assert (data["roadLength"] >= 0), "road cant be negative"
+            assert (data["repairsCost"] >= 0), "repairs cost cant be negative"
+            assert (data["repairsCost"] <= 20000), "repairs cost cant be greater than 20000"
 
-            hireStartTime = self.__str_time_prop(self.settings["birth_first_date"],
-                date_helper.strftime(date_format), date_format, random.random())
-            hireEndTime
-            carId
-            clientId
-            clientSatisfaction
-            roadLength
-            repairsCost
-        #datetime.now() + timedelta(days=1)
+            # writing hire
+            self.hireWriter.writerow([
+                data["hireStartTime"].strftime(_date_format),
+                data["hireEndTime"].strftime(_date_format),
+                data["carId"],
+                data["clientId"],
+                data["clientSatisfaction"],
+                data["roadLength"],
+                data["repairsCost"]
+            ])
 
-        self.__str_time_prop("1/1/2008 1:30 PM", "1/1/2009 4:50 AM", '%m/%d/%Y %I:%M %p', random.random())
-        self.writer.writerow(['Spam'] * 5 + ['Baked Beans'])
-        self.writer.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+        # writing client
+        self.clientWriter.writerow([
+            client["name"],
+            client["surname"],
+            client["birthDate"].strftime(_date_format),
+            client["gender"],
+            client["maritialStatus"],
+            client["numberOfChildrens"],
+            client["priceFactor"]
+        ])
 
 
     def __client_generate(self):
         # helpers
         date_format = self.settings["date_format"]
-        files_examples = self.settings["files_examples"]
-        date_helper = datetime.now() - timedelta(years=18)
+        birth_first_date = datetime.strptime(self.settings["birth_first_date"], date_format)
+        files_examples_num = self.settings["files_examples_num"]
 
         # data generation
-        birthDate = self.__str_time_prop(self.settings["birth_first_date"],
-                date_helper.strftime(date_format), date_format, random.random())
+        birthDate = self.__str_time_prop(birth_first_date, datetime.now() - relativedelta(years=20),
+            random.random())
         gender = "male" if random.randrange(2) == 0 else "female"
-        surname = self.__get_file_lines(self.settings["surnames_file"], random.randrange(files_examples))
+        surname = self.__get_file_lines(self.settings["surnames_file"], [random.randrange(files_examples_num)])[0]
         name = ""
         if gender == "male":
-            name = self.__get_file_lines(self.settings["male_names_file"], random.randrange(files_examples))
+            name = self.__get_file_lines(self.settings["male_names_file"], [random.randrange(files_examples_num)])[0]
         else:
-            name = self.__get_file_lines(self.settings["female_names_file"], random.randrange(files_examples))
+            name = self.__get_file_lines(self.settings["female_names_file"], [random.randrange(files_examples_num)])[0]
         maritialStatus = "married" if random.randrange(2) == 0 else "single"
         numberOfChildrens = random.randrange(5)
         priceFactor = float(random.randrange(50, 150)) / 100
 
         # to collection
         data = {
+            "id": self.clientID,
             "name": name,
-            "surname": surname,
+            "surname": surname.capitalize(),
             "birthDate": birthDate,
             "gender": gender,
             "maritialStatus": maritialStatus,
             "numberOfChildrens": numberOfChildrens,
             "priceFactor": priceFactor
         }
+
+        self.clientID += 1
+
+        # asserts
+        assert (data["name"] != ""), "Clients' name is empty"
+        assert (data["surname"] != ""), "Clients' surname is empty"
+        assert (data["birthDate"] > birth_first_date), "Client is too old"
+        assert (data["birthDate"] < datetime.now() - relativedelta(years=20)), "Client is too young"
+        assert (data["priceFactor"] < 1.51), "clients' price factor is bigger than 1,51"
+        assert (data["priceFactor"] > 0.49), "clients' price factor is lower than 0,49"
+
         return data
 
 
-    def __part2(self):
-        normalVar = pd.DataFrame(np.random.normal(0,10,70))
-        normalVar.columns = ['value']
-        normalVar.head()
-        print(normalVar.shape)
-        print(normalVar)
-
-
     def generate(self):
-        #self.__hire_generate()
-        self.__part2()
+        for i in tqdm(range(self.settings["clients_number"])):
+            self.__hire_generate()
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-#from libs.GeneratorCSV import GeneratorCSV
+from libs.GeneratorCSV import GeneratorCSV
 from datetime import datetime
 from datetime import timedelta
 import random
+import time
 from dateutil.relativedelta import relativedelta
 
 if __name__ == "__main__":
-    #generatorCSV = GeneratorCSV()
+    generatorCSV = GeneratorCSV("settings.json")
+    generatorCSV.generate()
     
-    #generatorCSV.generate()
-    datetime_helper1 = datetime.strptime("1/1/2008 1:30 PM", "%m/%d/%Y %I:%M %p")
-    datetime_helper1 = datetime_helper1 + relativedelta(years=18)
-    print(datetime_helper1.strftime("%m/%d/%Y %I:%M %p"))
-    #POPRAWIC TO W KODZIE!!!! (zamienic dodawanie czasu na relativetime)
+    #for i in range(50):
+    #    print(float(random.randrange(50, 150)) / 100)
 
     print("Done")
